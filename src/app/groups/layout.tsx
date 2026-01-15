0dc0a97e65827373b60348bc583348c9db44fc8a
Evan GRUCHOT <evangruchot.pro@gmail.com>
2026-01-15 17:25:33 +0100

    FIX/ URLS and descriptions

--- src/app/groups/layout.tsx
+++ src/app/groups/layout.tsx
@@ -5,7 +5,7 @@ import type {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: 'Trevn',
-    description: '',
+    description: undefined,
     robots: {
         index: false,
         follow: false,
