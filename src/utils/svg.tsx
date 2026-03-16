f80dfa0124811fa6c12593208913160c3ddc0447
Evan GRUCHOT <evangruchot.pro@gmail.com>
2026-03-16 17:07:03 +0100

    FIX/ typos

--- src/utils/svg.tsx
+++ src/utils/svg.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const HiddenEyeIcon = () => {
     return (
         <svg width="32" height="32" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
